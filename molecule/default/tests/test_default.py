22173d2ba3efb8d036f4f978a13a085e40f0ef2d
Lucas SAINTIER <lucas.saintier@fr.clara.net>
2021-12-10 16:36:10 +0100

    added test to certbot cron

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,9 +8,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
 
-def test_packages_is_installed(host):
+
+def test_certbot_is_installed(host):
     assert host.package('certbot').is_installed
 
 def test_cerbot_is_working(host):
     command = host.run("certbot --version")
     assert command.rc == 0
+
+def test_certbot_cron(host):
+    with host.sudo():
+        cron = host.check_output("crontab -l")
+        assert "certbot renew" in cron
