082e36df11658a3d3695ba8001eb0f59efa56039
Lucas SAINTIER <lucas.saintier@fr.clara.net>
2021-12-10 16:19:42 +0100

    added test to certbot packages

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,7 +8,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
 
+def test_packages_is_installed(host):
+    assert host.package('certbot').is_installed
 
-def test_shell(host):
-    command = host.run("sh --version")
+def test_cerbot_is_working(host):
+    command = host.run("certbot --version")
     assert command.rc == 0
